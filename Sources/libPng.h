e0f3f7c289b405ed05209a571325f8b2eb17ed71
Jakub Petrík <petrik@inloop.eu>
2016-07-23 11:32:09 +0200

    Initial commit

---
+++ Sources/libPng.h
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for libPng.
+FOUNDATION_EXPORT double libPngVersionNumber;
+
+//! Project version string for libPng.
+FOUNDATION_EXPORT const unsigned char libPngVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libPng/PublicHeader.h>
+
+#import <libPng/png.h>
+#import <libPng/pngconf.h>
